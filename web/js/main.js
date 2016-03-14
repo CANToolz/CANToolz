ba292e98051490845e826b620861b1b3ad7e2d58
Sergey Kononenko <kononencheg@gmail.com>
2016-03-14 19:05:37 +0300

    main menu and module

--- web/js/main.js
+++ web/js/main.js
@@ -10,40 +10,138 @@ var StepParams;
 var Step;
 
 /**
- * @typedef {{ queue: Array<!Step> }}
+ * @typedef {{queue: Array<!Step>, current: ?Step, running: boolean}}
  */
 var Scenario;
+
+/**
+ * @typedef {{}}
+ */
+var Module;
+
+/**
+ * @const {number}
+ */
+var STEP_HEIGHT_UNIT = 64;
+
+/**
+ * @const {number}
+ */
+var STEP_WIDTH_UNIT = 128;
+
+/**
+ * @type {!Object<string, !Module>}
+ */
+var moduleCache = {};
+
+/**
+ * No Operation Performed
+ */
+function nop() {}
+
+/**
+ * @param {!Step} step
+ * @param {function(!Module)} callback
+ */
+function stepModule(step, callback) {
+  var module = moduleCache[step.name];
+  if (module === undefined) {
+    d3.json('/api/help/', function(error, module) {
+      if (module !== null) {
+        callback(moduleCache[step.name] = module);
+      } else {
+        console.error(error);
+      }
+    });
+  } else {
+    callback(module);
+  }
+}
+
+/**
+ * @param {!Scenario} scenario
+ */
+function initControls(scenario) {
+  d3.select('.loop-action').on('click', function() {
+    if (scenario.running) {
+      scenario.running = false;
+      d3.json('/api/stop', nop);
+    } else {
+      scenario.running = true;
+      d3.json('/api/start', nop);
+    }
+
+    redrawHeader(scenario);
+  });
+
+  d3.select('.scenario').on('click', function() {
+    var target = d3.select(d3.event.target);
+    if (target.classed('step')) {
+      scenario.current = target.datum();
+
+      redrawCircuit(scenario);
+
+      stepModule(scenario.current, function(module) {
+        redrawMenu(module);
+      });
+    }
+  });
+}
+
+
+/**
+ * @param {!Module} module
+ */
+function redrawMenu(module) {
+
+}
+
+/**
+ * @param {!Scenario} scenario
+ */
+function redrawHeader(scenario) {
+  d3.select('.loop-action')
+      .classed('btn-success', !scenario.running)
+      .classed('btn-danger', scenario.running)
+}
+
+/**
+ * @param {!Scenario} scenario
+ */
+function redrawCircuit(scenario) {
+  var circuit = d3.select('.scenario')
+      .selectAll('.step')
+      .data(scenario.queue);
+
+  circuit
+      .enter()
+      .append('div')
+      .classed('step', true)
+      .classed('well', true)
+      .text(function (step) { return step.name })
+      .style('top', function(step, i) { return i * STEP_HEIGHT_UNIT + 'px' })
+      .style('left', function(step) { return (step.params.pipe - 1) * STEP_WIDTH_UNIT + 'px' })
+      .style('width', STEP_WIDTH_UNIT + 'px')
+      .style('height', STEP_HEIGHT_UNIT + 'px');
+
+  circuit
+      .classed('current', function(step) { return step === scenario.current; });
+}
 
 
 /**
  * @param {Error} error
  * @param {Scenario} scenario
  */
-function initScenario(error, scenario) {
+function init(error, scenario) {
   if (scenario !== null) {
-    var queue = scenario.queue;
+    scenario.current = null;
+    scenario.running = false;
 
-    var pipeScale = d3.scale.linear()
-        .domain(d3.extent(queue, function(step) { return step.params.pipe; }))
-        .range([0, 128]);
+    initControls(scenario);
 
-    var stepScale = d3.scale.linear()
-        .domain([0, queue.length])
-        .range([0, 720]);
-    
-    var circuit = d3.select('.application')
-        .selectAll('.step')
-        .data(queue);
-
-    circuit.enter()
-        .append('div')
-        .classed('step', true)
-        .text(function (s) { return s.name })
-        .style('top', function(s, i) { return stepScale(i) + 'px' })
-        .style('left', function(s) { return pipeScale(s.params.pipe) + 'px' })
-        
-
-
+    redrawHeader(scenario);
+    redrawCircuit(scenario);
   } else {
     console.error(error);
   }
@@ -53,5 +151,5 @@ function initScenario(error, scenario) {
  * Application bootstrap.
  */
 function main() {
-  d3.json('/api/get_conf', initScenario);
+  d3.json('/api/get_conf', init);
 }
