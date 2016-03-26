822417b1e89f4836b12323e449976bf5b7b58200
Alexey Sintsov <alex.sintsov@gmail.com>
2016-03-26 19:09:04 +0100

    Back config

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
-#  Load modules
 load_modules = {
-    'gen_replay': {'load_from':'ZeroTry'},
-    'mod_stat'  : {}
-
+    'hw_USBtin':    {'port':'auto', 'debug':1, 'speed':500},  # IO hardware module                           # Module for sniff and replay
+    'mod_stat':    {}                                         # Stats
 }
 
-
-# Scenario
-
+# Now let's describe the logic of this test
 actions = [
-    {'gen_replay'      : {'pipe': 1}},
-    {'mod_stat' : {'pipe': 1}}
-]
+    {'hw_USBtin':   {'action': 'read','pipe': 1}},   # Read to PIPE 1
+    {'mod_stat':    {'pipe': 1}}   # Write generated packets (pings)
+    ]
