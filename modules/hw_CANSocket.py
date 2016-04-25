b5b3ae3ff5473052142d538fc9bbd0c4f0885de7
Alexey Sintsov <alex.sintsov@gmail.com>
2016-04-25 16:09:38 +0200

    CANSocket add

--- modules/hw_CANSocket.py
+++ modules/hw_CANSocket.py
@@ -35,13 +35,22 @@ class hw_CANSocket(CANModule):
         self._run = False
 
     def dev_write(self, data):
-
+        self.dprint(1, "CMD: " + data)
+        if self._run:
+            idf, dataf = data.strip().split('#')
+            if idf[0:2]=="0x":
+                idf = int(idf, 16)
+            else:
+                idf = int(idf)
+            lenf = min(8, len(dataf))
+            self.do_write(CANMessage.init_data(idf, lenf, dataf.encode("ISO-8859-1")[0:lenf]))
         return ""
 
     def do_start(self, params):
-        if not self.device and not self._run:
+        if self.device and not self._run:
             try:
                 self.socket = socket.socket(socket.PF_CAN, socket.SOCK_RAW,  socket.CAN_RAW)
+                self.socket.setblocking(0)
                 self.socket.bind((self.device,))
                 self._run = True
             except Exception as e:
@@ -49,12 +58,13 @@ class hw_CANSocket(CANModule):
                 self.dprint(0, "ERROR: " + str(e))
 
     def do_stop(self, params):
-        if not self.device and self._run:
+        if self.device and self._run:
             try:
                 self.socket.close()
-                self._run = True
-            except:
                 self._run = False
+            except Exception as e:
+                self._run = False
+                self.dprint(0, "ERROR: " + str(e))
 
     def do_effect(self, can_msg, args):  # read full packet from serial port
         if args.get('action') == 'read':
@@ -67,16 +77,18 @@ class hw_CANSocket(CANModule):
 
     def do_read(self, can_msg):
         if self._run and not can_msg.CANData:
-            can_frame = self.socket.recv(16)
-            self.dprint(2, "READ: " + self.get_hex(can_frame))
-            #if len(can_frame) >:
-            #    can_msg.CANData = True
-            #    can_msg.CANFrame = CANMessage.init_raw_data(can_frame[0:4], can_frame[4], can_frame[5:])
-
+            try:
+                can_frame = self.socket.recv(16)
+                self.dprint(2, "READ: " + self.get_hex(can_frame))
+                if len(can_frame) == 16:
+                    can_msg.CANData = True
+                    can_msg.CANFrame = CANMessage.init_raw_data(struct.unpack("I", can_frame[0:4])[0], can_frame[4], can_frame[8:8+can_frame[4]])
+            except:
+                return can_msg
         return can_msg
 
     def do_write(self, can_msg):
         if can_msg.CANData:
-            cmd_byte = None
+            self.socket.write(struct.pack("I", can_msg.frame_id) + can_msg.frame_raw_data[0:can_msg.frame_length])
 
         return can_msg
