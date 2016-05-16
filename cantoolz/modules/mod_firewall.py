76bb96800a8b43656be11866e891020b0605f535
Alexey Sintsov <alex.sintsov@gmail.com>
2016-05-16 13:51:23 +0200

    ашч

--- cantoolz/modules/mod_firewall.py
+++ cantoolz/modules/mod_firewall.py
@@ -42,6 +42,14 @@ class mod_firewall(CANModule):
                 can_msg.CANData = False
                 self.dprint(2, "Message " + str(can_msg.CANFrame.frame_id) + " has been blocked(BB) (BUS = " + str(
                     can_msg.bus) + ")")
+            if 'hex_white_body' in args and can_msg.CANFrame.frame_raw_data.hex() not in args.get('hex_white_body',[]):
+                can_msg.CANData = False
+                self.dprint(2, "Message " + str(can_msg.CANFrame.frame_id) + " has been blocked(WB) (BUS = " + str(
+                    can_msg.bus) + ")")
+            elif 'hex_black_body' in args and can_msg.CANFrame.frame_raw_data.hex() in args.get('hex_black_body',[]):
+                can_msg.CANData = False
+                self.dprint(2, "Message " + str(can_msg.CANFrame.frame_id) + " has been blocked(BB) (BUS = " + str(
+                    can_msg.bus) + ")")
             if 'black_bus' in args and can_msg.bus.strip() in args.get('black_body',[]):
                 can_msg.CANData = False
                 self.dprint(2, "Message " + str(can_msg.CANFrame.frame_id) + " has been blocked(BBus) (BUS = " + str(
