f92a255f5aba4d429a407cb2b459997f09fe4ad9
Alexey Sintsov <alex.sintsov@gmail.com>
2017-02-01 14:52:58 +0100

    mod_stat fixes

--- examples/MIDI_to_CAN_example/CANToolz_config/BMW_F10_MUSIC.py
+++ examples/MIDI_to_CAN_example/CANToolz_config/BMW_F10_MUSIC.py
@@ -8,7 +8,24 @@ load_modules = {
 
 
 			# Music actions
+			
+			{'High light - blink': '0x1ee:2:20ff', 'cmd':'127'},
+			{'TURN LEFT and RIGHT': '0x2fc:7:31000000000000', 'cmd':'126'},
+			{'TURN right ON PERMANENT': '0x1ee:2:01ff', 'cmd':'124'},
+			{'TURN right x3 or OFF PERMANENT':'0x1ee:2:02ff','cmd':'123'},
+			{'TURN left ON PERMANENT': '0x1ee:2:04ff', 'cmd':'122'},
+			{'TURN left x3 or OFF PERMANENT':'0x1ee:2:08ff','cmd':'121'},
+			{'STOP lights': '0x173:8:3afa00022000f2c4', 'cmd':'120'},
 
+
+			{'CLEANER - once':'0x2a6:2:02a1','cmd':'119'},
+			{'CLEANER x3 + Wasser':'0x2a6:2:10f1','cmd':'118'},
+			{'Mirrors+windows FOLD':'0x26e:8:5b5b4002ffffffff','cmd':"117"},
+			{'Mirrors UNfold':'0x2fc:7:12000000000000','cmd':"116"},
+			{'Windows open':'0x26e:8:49494001ffffffff','cmd':"115"},
+			{'Windows rear close + Mirrors FOLD':'0x26e:8:405b4002ffffffff','cmd':"114"},
+			{'Trunk open':'0x2a0:8:88888001ffffffff','cmd':"113"},
+			{'Trunk close':'0x23a:8:00f310f0fcf0ffff','cmd':"112"}
 			
 
 		],
