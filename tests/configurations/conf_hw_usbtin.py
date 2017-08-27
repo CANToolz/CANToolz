0c868b2a4f44c80b5e6c47ceb529b3ee9bbd4f58
Tao Sauvage <sauvage.tao@gmail.com>
2017-08-27 08:32:33 +0200

    [tests] Move hw_usbtin tests to modules sub-directory. (#8)

---
+++ tests/configurations/conf_hw_usbtin.py
@@ -0,0 +1,11 @@
+# Test scenario/configuration
+load_modules = {
+    'hw_USBtin': {'port': 'loop', 'debug': 1, 'speed': 500},
+    'gen_replay': {'debug': 1, 'load_from': 'tests/data/test_responses.save'},                             # Module for sniff and replay
+    'mod_stat': {}}
+# The test scenario logic
+actions = [
+    {'hw_USBtin': {'action': 'read', 'pipe': 1}},
+    {'gen_replay': {'pipe': 2}},
+    {'mod_stat': {}},
+    {'hw_USBtin': {'action': 'write', 'pipe': 2}}]
--- tests/modules/test_hw_usbtin.py
+++ tests/modules/test_hw_usbtin.py
@@ -13,17 +13,19 @@ class ModUsbTin(unittest.TestCase):
 
     def test_usbtin(self):
         self.CANEngine = CANSploit()
-        self.CANEngine.load_config("tests/configurations/test_7.py")
+        self.CANEngine.load_config("tests/configurations/conf_hw_usbtin.py")
         self.CANEngine.start_loop()
         time.sleep(1)
-        self.CANEngine.call_module(1, "r")
-        self.CANEngine.call_module(3, "t T112233446112233445566")
+        # Replay loaded CAN messages via gen_replay
+        self.CANEngine.call_module(1, 'r')
+        # Send CAN message via hw_USBtin
+        self.CANEngine.call_module(3, 't T112233446112233445566')
         time.sleep(1)
-        ret = self.CANEngine.call_module(2, "p")
+        # Print table of sniffed CAN packets from mod_stat.
+        ret = self.CANEngine.call_module(2, 'p')
         _bodyList = self.CANEngine._enabledList[2][1]._bodyList
-        self.assertTrue(6 == len(_bodyList), "6 uiniq ID, should be sent")
+        self.assertTrue(6 == len(_bodyList), "6 unique ID, should be sent")
         self.assertTrue(0 <= ret.find(" 0x2bc "), "Message should be in the list")
         self.assertTrue(0 <= ret.find(" 2f410d0011223344 "), "Message should be in the list")
         self.assertTrue(0 <= ret.find(" 112233445566"), "Message should be in the list")
         self.assertTrue(0 <= ret.find(" 0x11223344 "), "Message should be in the list")
-
