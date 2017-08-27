798c5591435a2219df6f4e428cf546ee01855795
Tao Sauvage <sauvage.tao@gmail.com>
2017-08-27 08:19:31 +0200

    [tests] Move simple_io tests to modules sub-directory. (#8)

---
+++ tests/configurations/conf_simple_io.py
@@ -0,0 +1,13 @@
+# Test scenario/configuration
+load_modules = {
+    'hw_USBtin': {'port': 'loop', 'debug': 2, 'speed': 500},
+    'simple_io': {'bus': 'TEST_1'},
+    'mod_firewall': {},
+    'mod_stat': {}}
+# The test scenario logic
+actions = [
+    {'hw_USBtin': {'action': 'read', 'pipe': 1}},
+    {'simple_io': {}},
+    {'mod_stat': {}},
+    {'mod_firewall': {'white_bus': ['TEST_1']}},
+    {'hw_USBtin': {'action': 'write', 'pipe': 1}}]
