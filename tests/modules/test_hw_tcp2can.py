e11871b6ee39d487fcd522de7198df3674e647b0
Tao Sauvage <sauvage.tao@gmail.com>
2017-08-27 08:42:17 +0200

    [tests] Move hw_tcp2can tests to modules sub-directory. (#8)

--- tests/modules/test_hw_tcp2can.py
+++ tests/modules/test_hw_tcp2can.py
@@ -13,16 +13,20 @@ class TCP2CAN(unittest.TestCase):
 
     def test_server(self):
         self.CANEngine = CANSploit()
-        self.CANEngine.load_config("tests/configurations/test_13.py")
+        self.CANEngine.load_config("tests/configurations/conf_hw_tcp2can.py")
         self.CANEngine.start_loop()
         time.sleep(1)
-        self.CANEngine.call_module(3, "r ")
+        # Replay CAN messages via gen_replay.
+        self.CANEngine.call_module(3, 'r')
         time.sleep(1)
-        ret = self.CANEngine.call_module(6, "S ")
+        # Get status of mod_stats module.
+        ret = self.CANEngine.call_module(6, 'S')
         print(ret)
         self.assertTrue(0 < ret.find("index: 0 sniffed: 12"), "Should be be 12 packets")
-        self.CANEngine.call_module(8, "r ")
+        # Replay CAN messages via gen_replay~2.
+        self.CANEngine.call_module(8, 'r')
         time.sleep(1)
-        ret = self.CANEngine.call_module(1, "S ")
+        # Get status of mod_stats module.
+        ret = self.CANEngine.call_module(1, 'S')
         print(ret)
         self.assertTrue(0 < ret.find("index: 0 sniffed: 24"), "Should be be 12 packets")
