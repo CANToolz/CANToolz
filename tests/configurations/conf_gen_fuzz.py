2c577549a0d32127fd1583f6eaec500999c7f7b1
Tao Sauvage <sauvage.tao@gmail.com>
2017-08-27 08:26:51 +0200

    [tests] Move gen_fuzz tests to modules sub-directory. (#8)

---
+++ tests/configurations/conf_gen_fuzz.py
@@ -0,0 +1,11 @@
+# Test scenario/configuration
+load_modules = {
+    'gen_fuzz': {},
+    'pipe_switch': {},
+    'mod_stat': {}}
+# The test scenario logic
+actions = [
+    {'gen_fuzz': {}},
+    {'pipe_switch': {'action': 'read'}},
+    {'pipe_switch': {'action': 'write', 'pipe': 3}},
+    {'mod_stat': {'pipe': 3}}]
--- tests/modules/test_gen_fuzz.py
+++ tests/modules/test_gen_fuzz.py
@@ -4,7 +4,7 @@ import unittest
 from cantoolz.engine import CANSploit
 
 
-class ModFuzzTests(unittest.TestCase):
+class TestGenFuzz(unittest.TestCase):
 
     def tearDown(self):
         self.CANEngine.stop_loop()
@@ -13,21 +13,23 @@ class ModFuzzTests(unittest.TestCase):
 
     def test_fuzz_can(self):
         self.CANEngine = CANSploit()
-        self.CANEngine.load_config("tests/configurations/test_6.py")
+        self.CANEngine.load_config("tests/configurations/conf_gen_fuzz.py")
         self.CANEngine.edit_module(0, {'id': [111], 'data': [0, 1], 'bytes': (0, 4), 'delay': 0})
         self.CANEngine.start_loop()
         time.sleep(1)
-        self.CANEngine.call_module(0, "s")
+        # Start gen_fuzz module.
+        self.CANEngine.call_module(0, 's')
         time.sleep(3)
-        ret = self.CANEngine.call_module(3, "p")
+        # Print table of sniffed CAN packets from mod_stat.
+        ret = self.CANEngine.call_module(3, 'p')
         print(ret)
         index = 3
         _bodyList = self.CANEngine._enabledList[index][1]._bodyList
-        self.assertTrue(16 == len(_bodyList[111]), "16 uiniq packets, should be sent")
+        self.assertTrue(16 == len(_bodyList[111]), "16 unique packets should be sent")
 
     def test_fuzz_iso(self):
         self.CANEngine = CANSploit()
-        self.CANEngine.load_config("tests/configurations/test_6.py")
+        self.CANEngine.load_config("tests/configurations/conf_gen_fuzz.py")
         self.CANEngine.edit_module(
             0,
             {
@@ -40,10 +42,12 @@ class ModFuzzTests(unittest.TestCase):
             })
         self.CANEngine.start_loop()
         time.sleep(1)
-        self.CANEngine.call_module(0, "s")
+        # Start gen_fuzz module.
+        self.CANEngine.call_module(0, 's')
         time.sleep(3)
-        ret = self.CANEngine.call_module(3, "p")
+        # Print table of sniffed CAN packets from mod_stat.
+        ret = self.CANEngine.call_module(3, 'p')
         print(ret)
         index = 3
         _bodyList = self.CANEngine._enabledList[index][1]._bodyList
-        self.assertTrue(9 == len(_bodyList[111]), "9 uiniq packets, should be sent")
+        self.assertTrue(9 == len(_bodyList[111]), "9 unique packets should be sent")
