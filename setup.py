c6585e497aebef0bd280f9c2778a7d5c340c05d7
Tao Sauvage <sauvage.tao@gmail.com>
2017-08-27 17:51:22 +0200

    Bump to 3.6.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='CANToolz',
-    version='3.6.0',
+    version='3.6.1',
     description='Framework for black-box Controller Area Network (CAN) bus analysis.',
     author='Alexey Sintsov',
     author_email='alex.sintsov@gmail.com',
