53f801685c4c09afa13d4d62071ac42ffaf7dd8b
Tao Sauvage <sauvage.tao@gmail.com>
2017-08-27 10:12:14 +0200

    [tests] Merge all mod_stat tests to single test file. (#8)

---
+++ tests/configurations/conf_mod_stat.py
@@ -0,0 +1,12 @@
+# Test scenario/configuration
+load_modules = {
+    'hw_fakeIO': {'debug': 2, 'bus': 31},
+    'hw_fakeIO~1': {'debug': 2, 'bus': 32},
+    'mod_firewall': {'debug': 2},
+    'mod_stat': {'debug': 2}}
+# The test scenario logic
+actions = [
+    {'hw_fakeIO': {'action': 'write', 'pipe': 2}},
+    {'mod_stat': {'pipe': 1}},
+    {'mod_firewall': {'pipe': 2}},
+    {'hw_fakeIO~1': {'action': 'read', 'pipe': 2}}]
