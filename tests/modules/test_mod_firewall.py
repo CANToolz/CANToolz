ade2b25303b8f90ccdc15979907956223cb99ef2
Tao Sauvage <sauvage.tao@gmail.com>
2017-08-27 10:22:59 +0200

    [tests] Move mod_firewall tests to sub-directory. (#8)

--- tests/modules/test_mod_firewall.py
+++ tests/modules/test_mod_firewall.py
@@ -4,19 +4,21 @@ import unittest
 from cantoolz.engine import CANSploit
 
 
-class ModFirewallTests(unittest.TestCase):
+class TestModFirewall(unittest.TestCase):
 
     def tearDown(self):
         self.CANEngine.stop_loop()
+        self.CANEngine = None
+        print("stopped")
 
-    def test_blockedBodyHex(self):
+    def test_blocked_body_hex(self):
         self.CANEngine = CANSploit()
-        self.CANEngine.load_config('tests/configurations/test_1.py')
+        self.CANEngine.load_config('tests/configurations/conf_mod_stat.py')
         self.CANEngine.edit_module(2, {'pipe': 2, 'hex_black_body': ['0102030605']})
         self.CANEngine.start_loop()
         index = 3
 
-        self.CANEngine.call_module(0, "t 4:6:010203060505")  # pass
+        self.CANEngine.call_module(0, 't 4:6:010203060505')  # pass
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertFalse(len(mod) == 0, "We should find message in PIPE")
@@ -24,104 +26,100 @@ class ModFirewallTests(unittest.TestCase):
 
         self.CANEngine._enabledList[index][1].CANList = []
 
-        self.CANEngine.call_module(0, "t 4:5:0102030605")  # blocked
+        self.CANEngine.call_module(0, 't 4:5:0102030605')  # blocked
 
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertTrue(len(mod) == 0, "We should NOT find message in PIPE")
-        #self.assertFalse(mod.frame_id == 4, "We should be able to find ID 4")
         self.CANEngine._enabledList[index][1].CANList = []
 
         self.CANEngine.edit_module(2, {'pipe': 2, 'hex_white_body': ['0102030605']})
 
-        self.CANEngine.call_module(0, "t 4:5:0102030605")  # pass
+        self.CANEngine.call_module(0, 't 4:5:0102030605')  # pass
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertFalse(len(mod) == 0, "We should find message in PIPE")
         self.assertTrue(mod[-1].frame_id == 4, "We should be able to find ID 4")
         self.CANEngine._enabledList[index][1].CANList = []
 
-        self.CANEngine.call_module(0, "t 4:6:010203060505")  # blocked
+        self.CANEngine.call_module(0, 't 4:6:010203060505')  # blocked
 
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertTrue(len(mod) == 0, "We should NOT find message in PIPE")
-        #self.assertFalse(mod.frame_id == 4, "We should be able to find ID 4")
         self.CANEngine._enabledList[index][1].CANList = []
 
-    def test_blockedBody(self):
+    def test_blocked_body(self):
         self.CANEngine = CANSploit()
-        self.CANEngine.load_config('tests/configurations/test_1.py')
+        self.CANEngine.load_config('tests/configurations/conf_mod_stat.py')
         self.CANEngine.edit_module(2, {'pipe': 2, 'black_body': [[1, 2, 3, 6, 5]]})
         self.CANEngine.start_loop()
         index = 3
 
-        self.CANEngine.call_module(0, "t 4:6:010203060505")  # pass
+        self.CANEngine.call_module(0, 't 4:6:010203060505')  # pass
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertFalse(len(mod) == 0, "We should find message in PIPE")
         self.assertTrue(mod[-1].frame_id == 4, "We should be able to find ID 4")
         self.CANEngine._enabledList[index][1].CANList = []
 
-        self.CANEngine.call_module(0, "t 4:5:0102030605")  # blocked
+        self.CANEngine.call_module(0, 't 4:5:0102030605')  # blocked
 
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertTrue(len(mod) == 0, "We should NOT find message in PIPE")
-        #self.assertFalse(mod.frame_id == 4, "We should be able to find ID 4")
         self.CANEngine._enabledList[index][1].CANList = []
 
         self.CANEngine.edit_module(2, {'pipe': 2, 'white_body': [[1, 2, 3, 6, 5]]})
 
-        self.CANEngine.call_module(0, "t 4:5:0102030605")  # pass
+        self.CANEngine.call_module(0, 't 4:5:0102030605')  # pass
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertFalse(len(mod) == 0, "We should find message in PIPE")
         self.assertTrue(mod[-1].frame_id == 4, "We should be able to find ID 4")
         self.CANEngine._enabledList[index][1].CANList = []
 
-        self.CANEngine.call_module(0, "t 4:6:010203060505")  # blocked
+        self.CANEngine.call_module(0, 't 4:6:010203060505')  # blocked
 
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertTrue(len(mod) == 0, "We should NOT find message in PIPE")
-        #self.assertFalse(mod.frame_id == 4, "We should be able to find ID 4")
         self.CANEngine._enabledList[index][1].CANList = []
 
     def test_blockedID(self):
         self.CANEngine = CANSploit()
-        self.CANEngine.load_config('tests/configurations/test_1.py')
+        self.CANEngine.load_config('tests/configurations/conf_mod_stat.py')
         self.CANEngine.edit_module(2, {'pipe': 2, 'black_list': [1, 2, 3, 6, 5]})
         self.CANEngine.start_loop()
         index = 3
 
-        self.CANEngine.call_module(0, "t 4:4:11223344")  # pass
+        self.CANEngine.call_module(0, 't 4:4:11223344')  # pass
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertFalse(len(mod) == 0, "We should find message in PIPE")
         self.assertTrue(mod[-1].frame_id == 4, "We should be able to find ID 4")
         self.CANEngine._enabledList[index][1].CANList = []
 
-        self.CANEngine.call_module(0, "t 1:4:11223344")
+        self.CANEngine.call_module(0, 't 1:4:11223344')
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertFalse(len(mod) > 0, "Message number 1 should not pass")
         self.CANEngine._enabledList[index][1].CANList = []
 
-        self.CANEngine.call_module(0, "t 7:4:11223344")  # pass
+        self.CANEngine.call_module(0, 't 7:4:11223344')  # pass
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertTrue(mod[-1].frame_id == 7, "We should be able to find ID 7")
         self.CANEngine._enabledList[index][1].CANList = []
 
-        self.CANEngine.call_module(0, "t 1:4:11223344")
+        self.CANEngine.call_module(0, 't 1:4:11223344')
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertFalse(len(mod) > 0, "Message number 1 should not pass")
         self.CANEngine._enabledList[index][1].CANList = []
-        self.CANEngine.call_module(0, "t 1:8:1122334411223344")
+        self.CANEngine.call_module(0, 't 1:8:1122334411223344')
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertFalse(len(mod) > 0, "Message number 1 should not pass")
         self.CANEngine._enabledList[index][1].CANList = []
 
-        self.CANEngine.call_module(0, "t 4:4:11223344")  # pass
+        self.CANEngine.call_module(0, 't 4:4:11223344')  # pass
         time.sleep(1)
         mod = self.CANEngine._enabledList[index][1].CANList
         self.assertTrue(mod[-1].frame_id == 4, "We should be able to find ID 4")
