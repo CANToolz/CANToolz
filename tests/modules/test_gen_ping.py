206ee4df3395fb85bf1f87e146d5f6132c601f89
Tao Sauvage <sauvage.tao@gmail.com>
2017-08-27 10:17:51 +0200

    [tests] Merge modgenpingex with gen_pint tests. (#8)

--- tests/modules/test_gen_ping.py
+++ tests/modules/test_gen_ping.py
@@ -87,3 +87,63 @@ class TestGenPing(unittest.TestCase):
             "24a1a2a3a4a5a6a7" == (codecs.encode((list(_bodyList[543001].keys())[4][1]), 'hex_codec')).decode("ISO-8859-1"),
             "Last packet of sec should be like that"
         )
+
+    def test_ping_uds(self):
+        self.CANEngine = CANSploit()
+        self.CANEngine.load_config('tests/configurations/conf_gen_replay_uds.py')
+        self.CANEngine.edit_module(
+            2,
+            {
+                'pipe': 2,
+                'services': [
+                    {"service": 1, "sub": [1, 2]},
+                    {"service": [2, 3], "sub": 3},
+                    {"service": "0x4-6", "sub": "0x4-6"}],
+                'mode': 'UDS',
+                'range': [1, 2]
+            })
+        time.sleep(1)
+        self.CANEngine.start_loop()
+        time.sleep(1)
+        # Start gen_ping module.
+        self.CANEngine.call_module(2, 's')
+        time.sleep(1)
+        mod_stat = 3
+        # Print count of loaded packets.
+        ret1 = self.CANEngine.call_module(mod_stat, 'p 0')
+        print(ret1)
+        self.assertTrue(0 <= ret1.find('020505'), "Should be found")
+        self.assertTrue(0 <= ret1.find('020504'), "Should be found")
+        self.assertTrue(0 <= ret1.find('020405'), "Should be found")
+        self.assertTrue(0 <= ret1.find('020404'), "Should be found")
+        self.assertTrue(0 <= ret1.find('020303'), "Should be found")
+        self.assertTrue(0 <= ret1.find('020203'), "Should be found")
+        self.assertTrue(0 <= ret1.find(' 020102 '), "Should be found")
+        self.assertTrue(0 <= ret1.find(' 020101 '), "Should be found")
+        self.assertFalse(0 <= ret1.find(' 0101 '), "Should not be found")
+
+    # https://github.com/eik00d/CANToolz/issues/93 by @DePierre
+    def test_ping_uds_none_crash(self):
+        self.CANEngine = CANSploit()
+        self.CANEngine.load_config('tests/configurations/conf_gen_replay_uds.py')
+        self.CANEngine.edit_module(
+            2,
+            {
+                'pipe': 2,
+                'services': [
+                    {"service": 1, "sub": None}],
+                'mode': 'UDS',
+                'range': [1, 2]
+            })
+        time.sleep(1)
+        self.CANEngine.start_loop()
+        time.sleep(1)
+        # Start gen_ping module.
+        self.CANEngine.call_module(2, 's')
+        time.sleep(1)
+        mod_stat = 3
+        # Print count of loaded packets.
+        ret1 = self.CANEngine.call_module(mod_stat, 'p 0')
+        print(ret1)
+        self.assertTrue(0 <= ret1.find('0x1'), "Should be found")
+        self.assertTrue(0 <= ret1.find('0101'), "Should be found")
